4eac89e9ab939c44662c4e712fcd074255fd0800
jayasujaiswal <jayasujaiswal@gmail.com>
2024-05-20 11:49:34 +0530

    Add files via upload

---
+++ calender.py
@@ -0,0 +1,15 @@
+
+#import calendar
+#year=int(input("Enter the year:"))
+#month=int(input("Enter the month:"))
+#cal=calendar.month(year,month)
+#print(cal)
+
+
+
+
+import calendar
+year=int(input("Enter the year:"))
+
+cal=calendar.calendar(year)
+print(cal)
