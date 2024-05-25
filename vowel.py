2a50f787334d3576ceacf735d7cfade987510949
jayasujaiswal <jayasujaiswal@gmail.com>
2024-05-25 08:20:40 +0530

    check_consonant&vowel

---
+++ vowel.py
@@ -0,0 +1,5 @@
+ch=input("enter any character to check whether for vowel and consonant:::::")
+if ch =='a'or ch=='e'or ch=='i'or ch=='o'or ch=='u' or ch=='A'or ch=='E' or ch=='I' or ch=='O'or ch=='U' :
+    print("Character is VOWEL!!!!")
+else :
+    print("Character is CONSONANT!!!!")
