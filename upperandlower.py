f3cdf546d58aa909da0442461a17e4350ad4efa7
jayasujaiswal <jayasujaiswal@gmail.com>
2024-05-25 08:36:30 +0530

    case
    To check whether the character in lower or upper case

---
+++ upperandlower.py
@@ -0,0 +1,7 @@
+char=input("enter any character")
+if char >='A' and char <='Z':
+    print("Upper case")
+elif char >= 'a' and char<='z':
+    print("Lower case")
+else:
+    print("you an pass only a to z or A to Z only ")
